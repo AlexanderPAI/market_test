2a15d2474a438259db2f8e4662d69098e1c03c85
AlexanderPAI <petrov@backendpages.ru>
2026-02-09 17:37:50 +0300

    First attempt migrations

---
+++ src/infrastructure/db/models/__init__.py
@@ -0,0 +1,9 @@
+from src.infrastructure.db.models.base import Base  # noqa
+from src.infrastructure.db.models.market import (  # noqa
+    Category,
+    CategoryRels,
+    Client,
+    Order,
+    OrderProducts,
+    Product,
+)
